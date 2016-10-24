329def2d80aba1017f2155782cee507e553eeaa3
Joker Qyou <Joker.Qyou@gmail.com>
2016-10-24 20:55:16 +0800

    Fix daily reset

--- keycounter/base_counter.py
+++ keycounter/base_counter.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from datetime import datetime, timedelta
+from datetime import datetime
 import logging
 
 from .storage import CountDataStorage
@@ -24,7 +24,7 @@ class BaseKeyCounter(object):
         self.key_count = 0
         self.daily_reset = True
 
-        self.today = datetime.now().day
+        self.today = datetime.now()
         self.setup_storage()
 
     def setup_storage(self):
@@ -55,8 +55,7 @@ class BaseKeyCounter(object):
         '''Reset count and start a new day'''
         self.log('Perform daily reset')
         yesterday_count = self.key_count - 1
-        yesterday = datetime.now() - timedelta(1)
-        self.save_data(yesterday, yesterday_count)
+        self.save_data(self.day, yesterday_count)
         self.storage.export()
         self.key_count -= yesterday_count
         if self.key_count < 0:
@@ -74,9 +73,9 @@ class BaseKeyCounter(object):
     def check_daily_reset(self):
         '''Check whether it's time to do daily reset'''
         now = datetime.now()
-        if now.day != self.today:
+        if now.day != self.today.day:
             self.do_daily_reset()
-            self.today = now.day
+            self.today = now
 
     def update_count(self):
         '''Update count and reflect the change to UI'''
