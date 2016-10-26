c888dd1937e667132225b8683fe786d4d7b0c218
Joker Qyou <Joker.Qyou@gmail.com>
2016-10-26 21:18:42 +0800

    Add daily reset unit test

--- tests/test_base_counter.py
+++ tests/test_base_counter.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from datetime import datetime, timedelta
 import unittest
 
 from keycounter.base_counter import BaseKeyCounter
@@ -11,7 +12,6 @@ class BaseKeyCounterTest(unittest.TestCase):
     def setUp(self):
         self.counter = BaseKeyCounter()
         self.counter.update_ui = lambda: None
-        self.counter.start()
         # We'll reset the count to init value so we don't mess up real data
         self.init_count = self.counter.key_count
 
@@ -22,6 +22,7 @@ class BaseKeyCounterTest(unittest.TestCase):
         del self.counter
 
     def test_init(self):
+        self.counter.start()
         self.assertEqual(
             self.counter.key_count,
             self.init_count,
@@ -29,6 +30,7 @@ class BaseKeyCounterTest(unittest.TestCase):
         )
 
     def test_save_load(self):
+        self.counter.start()
         init_count_backup = self.init_count
 
         self.counter.update_count()
@@ -36,6 +38,7 @@ class BaseKeyCounterTest(unittest.TestCase):
         self.counter.stop()
 
         self.setUp()
+        self.counter.start()
         self.assertEqual(
             self.counter.key_count,
             last_count,
@@ -45,9 +48,39 @@ class BaseKeyCounterTest(unittest.TestCase):
         self.init_count = init_count_backup
 
     def test_daily_reset(self):
-        pass
+        self.assertTrue(
+            self.counter.daily_reset,
+            'BaseKeyCounter.daily_reset should be True'
+        )
+        # Set `today` to yesterday and manually trigger daily reset
+        yesterday = datetime.now() + timedelta(-1)
+        self.counter.today = yesterday
+        self.counter.start()
+        yesterday_count_backup = self.counter.key_count
+
+        self.counter.handle_keyevent(None)
+        self.assertEquals(
+            self.counter.key_count,
+            1,
+            'Count should be 1 after a daily reset'
+        )
+        self.assertEqual(
+            self.counter.today.day,
+            datetime.now().day,
+            'BaseKeyCounter.today should update after a daily reset'
+        )
+        self.assertEqual(
+            yesterday_count_backup,
+            self.counter.storage.get(yesterday),
+            'Count of the last day should be saved'
+        )
+
+        # Restore count of yesterday so we don't mess up real data
+        self.counter.today = yesterday
+        self.counter.key_count = yesterday_count_backup
 
     def test_handle_keyevent(self):
+        self.counter.start()
         start_count = self.counter.key_count
         self.counter.handle_keyevent(None)
         self.assertEqual(
@@ -57,6 +90,7 @@ class BaseKeyCounterTest(unittest.TestCase):
         )
 
     def test_update_count(self):
+        self.counter.start()
         start_count = self.counter.key_count
         self.counter.update_count()
         self.assertEqual(
